a0fbc915d1cf74264e6d313231b5b86f3a71b73f
Shiheuan <shiheuan@gmail.com>
2019-07-15 16:05:54 +0800

    0x01 RunString

---
+++ TestMoonSharp/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoonSharp.Interpreter;
+
+namespace TestMoonSharp
+{
+    class Class1
+    {
+        public double MoonSharpFactorial()
+        {
+            string script = @"
+                -- defines a factorial function
+                function fact (n)
+                    if (n == 0) then
+                        return 1
+                    else
+                        return n*fact(n-1)
+                    end
+                end
+
+                return fact(5)";
+            DynValue res = Script.RunString(script);
+            Console.WriteLine(res.Number);
+            Console.ReadKey();
+            return res.Number;
+        }
+    }
+}
---
+++ TestMoonSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMoonSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Class1();
+            a.MoonSharpFactorial();
+        }
+    }
+}
